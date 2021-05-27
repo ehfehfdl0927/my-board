24b46cb8a5978282bf174ca4ebc2407f3a3d5609
ehfehfdl0927 <kimjoohe10@naver.com>
2021-05-27 17:11:20 +0900

    커뮤니티

---
+++ src/main/java/kimjoohe/myboard/controller/SearchForm.java
@@ -0,0 +1,22 @@
+package kimjoohe.myboard.controller;
+
+public class SearchForm {
+    private String search_option;
+    private String keyword;
+
+    public String getSearch_option() {
+        return search_option;
+    }
+
+    public void setSearch_option(String search_option) {
+        this.search_option = search_option;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
